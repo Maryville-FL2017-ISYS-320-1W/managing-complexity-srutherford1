178c89ded2a737fc1a1c39c1d549526d14a81761
Samantha Rutherford <33076449+srutherford1@users.noreply.github.com>
2017-11-12 21:27:01 -0500

    Update P1_OutputLoopResult.java

--- src/P1_OutputLoopResult.java
+++ src/P1_OutputLoopResult.java
@@ -2,7 +2,7 @@
   	ISYS 320
   	Name(s):
   	Date: 
-*/
+
 
 
 
@@ -20,5 +20,33 @@ public class P1_OutputLoopResult {
 		
 		System.out.println( "Final result is: " + finalResult );
 	}
+	System.out.println should be inside of the brackets for for loop because final Results is on declared within the for loop.
 
 }
+*/
+/*
+  	ISYS 320
+  	Name(s): Samantha Rutherford
+  	Date: 11/12/2017
+*/
+
+
+
+public class P1_OutputLoopResult {
+
+	public static void main(String[] args) {
+		printSumOfSquaresTo5();
+	}
+
+	public static void printSumOfSquaresTo5() {
+
+		for( int i = 1; i <= 5; i++ ) {
+			int finalResult = 0;
+			finalResult = finalResult + i * i;
+			System.out.println( "Final result is: " + finalResult);
+		}
+
+	}
+
+}
+
